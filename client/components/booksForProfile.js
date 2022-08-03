176d56dcbb1961f01d8c8341c741c51f0e627686
Tyler Wilson <wilsontyler95@gmail.com>
2022-08-03 13:29:53 -0400

    Added unlike feature on profile and added update/delete buttons with no functionality

--- client/components/booksForProfile.js
+++ client/components/booksForProfile.js
@@ -3,11 +3,27 @@ import Comment from './Comments'
 import { useStoreState, useStoreActions } from 'easy-peasy';
 
 export default function ProfileBooks(props) {
+  const user = useStoreState((state) => state.user);
+  const updateUser = useStoreActions((actions) => actions.updateUser);
+
+  async function handleUnlike(isbn) {
+    const sendingInfo = { email: user.email, isbn: isbn };
+    const result = await fetch('/books/unLike', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(sendingInfo)
+    })
+    .then((data) => data.json())
+    .then((data) =>
+      updateUser(data))
+  }
+
   return (
     <div>
       <h4>Book Name: {props.book.name} </h4>
       <img src={props.book.imageUrl} />
       <h4>ISBN-10: {props.book.isbn}</h4>
+      <button className="removeBook" onClick={() => handleUnlike(props.book.isbn)}>Remove from favorites</button>
 
       <h4>Description: {props.book.description}</h4>
       <a href={props.book.moreInfo}>More Info</a>
