7364f1e183c5c50737d2ab32c48d0756c44ed5ba
Kevin Le <kevinle1003@gmail.com>
2022-08-03 14:23:43 -0500

    enter keydown wip

--- client/pages/Search.js
+++ client/pages/Search.js
@@ -8,6 +8,7 @@ export default function search() {
   //display the response from api
   // the book have like button and unlike button
   const [books, setBooks] = useState([])
+  const [input, setInput] = useState('');
   // AIzaSyCII8FgqP289MMPY4J0rvIotk0mYT-eqLA
   async function queryBooks(keyWords) {
     const url = `https://www.googleapis.com/books/v1/volumes?&q=${keyWords}&key=AIzaSyCII8FgqP289MMPY4J0rvIotk0mYT-eqLA`;
@@ -28,9 +29,11 @@ export default function search() {
 
   const handleKeyDown = (e) => {
     if(e.key === 'Enter') {
-      queryBooks(document.getElementById('search').value);
+      queryBooks(e.target.value);
     }
   }
+
+  // console.log(input)
 
   return (
 
@@ -40,8 +43,7 @@ export default function search() {
 
       <h1 className='searchbar'>Search for a book!</h1>
       <form className='searchbar'>
-        <input onKeyDown={handleKeyDown} type='text' id="search" placeholder='My favorite title'></input>
-        <label htmlFor="search"></label>
+        <input onKeyDown={(e) => handleKeyDown(e)} type='text' id="search" placeholder='My favorite title'></input>
         <button className='search-button' type='button' onClick={() => { queryBooks(document.getElementById('search').value) }}>Search</button>
       </form>
 
